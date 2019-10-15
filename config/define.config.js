9cedb3b45a5db7832ad0333af8953a906b1df921
Nguyen Anh Van <anhvandev.it@gmail.com>
2019-10-15 10:34:43 +0700

    update

--- config/define.config.js
+++ config/define.config.js
@@ -5,3 +5,4 @@ global.hepler_path = app_root + '/app/helper/';
 global.view_path = app_root + '/view/';
 global.public_path = app_root + '/public/';
 global.static_path = app_root + '/static/';
+global.pmw_path = app_root + '/static/';
---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'MainApp',
+    script: 'app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    instances: 1,
+    autorestart: true,
+    //watch: false,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '168.63.148.223',
+      ref  : 'origin/master',
+      repo : 'git@github.com:anhvandev/web_003.git',
+      path : '/home/fb.toolvi.tk/public_html',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
